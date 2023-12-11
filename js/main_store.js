041467c5d30c5906422218f0ece6f55bbd258f1e
Gustavo Trujillo <gustavoetrujilloc@gmail.com>
2023-12-12 00:11:48 +0100

    Preparacion de 3ra entrega, se agrego carrito de compras a la tienda.

--- js/main_store.js
+++ js/main_store.js
@@ -3,18 +3,66 @@
 class Juegos {
   constructor(nombre, precio, plataforma, img) {
       this.nombre = nombre;
-      this.precio = precio;
+      this.precio = Number(precio);
       this.plataforma = plataforma;
       this.img = img;
   }
 }
 
-let fc24 = new Juegos("FC 24", "80 $", "Ps5", "fc24.webp");
-let acm = new Juegos("Assassins Creed® Mirage", "70 $", "Ps5", "acm.webp");
-let battlefield = new Juegos("Battlefield 2042", "60 $", "Ps5", "battlefield.webp");
-let codmw = new Juegos("Call of Duty: Modern Warfare® III", "500 $", "Ps5", "callofduty3.webp");
+let fc24 = new Juegos("FC 24", 80, "Ps5", "fc24.webp");
+let acm = new Juegos("Assassins Creed® Mirage", 70, "Ps5", "acm.webp");
+let battlefield = new Juegos("Battlefield 2042", 60, "Ps5", "battlefield.webp");
+let codmw = new Juegos("Call of Duty: Modern Warfare® III", 500, "Ps5", "callofduty3.webp");
 
 const JUEGOS = [fc24, acm, battlefield, codmw];
+
+//Funcion para agregar los productos del carrito de compras al localStorage
+
+function agregarAlCarrito(nombre, precio, plataforma, img) {
+  const CARRITO = JSON.parse(localStorage.getItem('carrito')) || [];
+  const juego = new Juegos(nombre, precio, plataforma, img);
+  CARRITO.push(juego);
+  localStorage.setItem('carrito', JSON.stringify(CARRITO));
+  mostrarCarrito();
+}
+
+//Funcion para agregar al carrito de compras los productos
+
+function mostrarCarrito()  {
+const CARRITO = JSON.parse(localStorage.getItem('carrito')) || [];
+const listaJuegos = document.getElementById('listaJuegos');
+const totalJuegosSpan = document.getElementById('totalJuegos');
+let totalJuegos = 0;
+
+listaJuegos.innerHTML = '';
+
+CARRITO.forEach(juego => {
+  const listItem = document.createElement('li');
+  listItem.innerHTML = `
+                <span>${juego.nombre}</span>
+                <span>${juego.precio}</span>
+                <span>${juego.plataforma}</span>
+                <span>${juego.img}</span>     
+    `
+    listaJuegos.appendChild(listItem);
+
+    totalJuegos += parseFloat(juego.precio);
+});
+
+totalJuegosSpan.textContent = totalJuegos.toFixed(2);
+
+};
+
+//Funcion para vaciar carrito de comprar
+
+
+const vaciarCarrito = document.getElementById('vaciar');
+vaciarCarrito.addEventListener('click', function vaciarCarrito(){
+  localStorage.removeItem('carrito');
+  mostrarCarrito();
+  });
+
+//Funcion para generar tarjetas de productos
 
 JUEGOS.forEach(Juegos => {
   let div= document.createElement("div"); 
@@ -32,8 +80,7 @@ JUEGOS.forEach(Juegos => {
   div2.className = "contentBx";
   div2.innerHTML= `<h2>${Juegos.nombre}</h2>
                     <h3><span>${Juegos.precio}</span></h3>
-                    <a>Comprar</a>
-                   
+                    <a onclick="agregarAlCarrito('${Juegos.nombre}', ${Juegos.precio}, '${Juegos.plataforma}', '${Juegos.img}')">Comprar</a>
                     `;
 
   div.appendChild(div2);
@@ -48,5 +95,10 @@ JUEGOS.forEach(Juegos => {
 
   
 })
+
+
+
+agregarAlCarrito();
+mostrarCarrito();
 
 
